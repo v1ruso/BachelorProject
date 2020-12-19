3d98588cc448d5d9c09f5ad1be207c43d8247141
Sebastian Velez de Villa <sebastian.velezdevilla@epfl.ch>
2020-12-19 18:40:18 +0100

    Added midi approximate pattern

--- Markov_model/utils/approximate_pattern_midi.py
+++ Markov_model/utils/approximate_pattern_midi.py
@@ -1,37 +1,19 @@
 import numpy as np
-
-def hamming_distance(seq_1,seq_2):
-    return sum(c1 != c2 for c1, c2 in zip(seq_1, seq_2))
-
-def edit_distance(s1, s2):
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
-
-    distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
-    return distances[-1]
-
+import pretty_midi
 def find_approximate_patterns(seq_notes):
     """
-    based on SIA(TEC) algorithm
-    seq_notes: list of (onset,pitch) elements
+    seq_notes: a list a pretty_midi notes
+    based on SIA(tec) algorithm
     """
     vector_matrix = np.empty((len(seq_notes),len(seq_notes)),dtype=object)
     for i in range(len(seq_notes)): # rows
         if seq_notes[i]==None:
             continue
-        for j in range(len(seq_notes)): # columns
+        for j in range(len(seq_notes)): # columns
             if seq_notes[j]==None:
                 continue
             if j<i:
-                vector_matrix[i,j] = (seq_notes[i][0]-seq_notes[j][0],seq_notes[i][1]-seq_notes[j][1])
+                vector_matrix[i,j] = (seq_notes[i].start-seq_notes[j].start,seq_notes[i].pitch-seq_notes[j].pitch)
             else:
                 vector_matrix[i,j] = (0,0)
     result = {}
@@ -49,33 +31,25 @@ def find_approximate_patterns(seq_notes):
                 else:
                     result[vector_matrix[i,j]] = list()
                     result[vector_matrix[i,j]].append(seq_notes[j])
-    """mtp_head = (0,0)
-    mtp_datapoints = list()
-    max_length = 0
-    for vector in result:
-        if len(result[vector])>max_length:
-            max_length=len(result[vector])
-            mtp_datapoints = result[vector]
-            mtp_head = vector"""
     return result
 
 def is_note_in_seq(note,seq):
     for n in seq:
-        if n[0] == note[0] and n[1]==note[1]:
+        if n.start == note.start and n.pitch == note.pitch:
             return True
     return False
-
 def are_seqs_equal(seq_1,seq_2):
     if len(seq_1)!=len(seq_2):
         return False
     for i in range(len(seq_1)):
-        if seq_1[i][0]!=seq_2[i][0] or seq_1[i][1]!=seq_2[i][1]:
+        if seq_1[i].start!=seq_2[i].start or seq_1[i].pitch!=seq_2[i].pitch:
             return False
     return True
+
 def filter_patterns(patterns, notes):
     """
-    patterns: dictionary, keys are translation vectors, values are notes
-        which can be transformed into other notes using the key
+    patterns: dictionary, keys are translation vectors, values are notes which 
+        can be transformed into other notes using the key
     """
     # first transform list of notes into dictionary
     new_patterns = {}
@@ -83,25 +57,24 @@ def filter_patterns(patterns, notes):
         # transform list of notes from pattern into dictionary
         temp_notes = {}
         for n in patterns[key]:
-            temp_notes[n[0]]=n[1]
+            temp_notes[n.start] = n.pitch
         temp_pattern = list()
         # filter patterns so that a note isn't repeated twice within a pattern
         for n in patterns[key]:
-            new_note = (n[0]+key[0],n[1]+key[1])
-            if new_note[0] not in temp_notes:
+            if (n.start + key[0]) not in temp_notes:
                 temp_pattern.append(n)
         # now we need to check if all elements are contiguous within a pattern
         new_patterns[key] = list()
         for i in range(len(notes)):
             if notes[i]==None:
                 continue
-            if notes[i][0] == temp_pattern[0][0] and notes[i][1] == temp_pattern[0][1]:
+            if notes[i].start == temp_pattern[0].start and notes[i].pitch == temp_pattern[0].pitch:
                 for j in range(0,min(len(notes),len(temp_pattern))):
-                    if notes[j+i][0] == temp_pattern[j][0] and notes[j+i][1] == temp_pattern[j][1]:
+                    if notes[j+i].start == temp_pattern[j].start and notes[j+i].pitch == temp_pattern[j].pitch:
                         new_patterns[key].append(temp_pattern[j])
                     else:
                         break
-    # now remove empty entries
+    # now remove whose size is not greater than 1
     result = {}
     for key in new_patterns:
         if len(new_patterns[key])>1:
@@ -116,17 +89,16 @@ def find_biggest_pattern_in_patterns(dict):
             max_length=len(dict[key])
             trans_vector = key
             pattern = dict[key]
-    return pattern, trans_vector
-def find_all_trans_vector_with_pattern(dict,pattern):
+    return pattern,trans_vector
+def find_all_trans_vector_with_pattern(dict, pattern):
     ret = list()
     for key in dict:
         if are_seqs_equal(pattern,dict[key]):
             ret.append(key)
     return ret
-
 def find_pattern_with_indices(seq,list_patterns,pattern_to_indices,index_pattern):
     """
-    seq: list of (onset,pitch) elements
+    seq: list of pretty midi notes
     pattern_to_indices: already existent patterns
     index_pattern: key where to add the new pattern, i.e. pattern_to_indices[index_pattern] = ...
     This method is supposed to find one pattern.
@@ -137,20 +109,18 @@ def find_pattern_with_indices(seq,list_patterns,pattern_to_indices,index_pattern
     if pattern!=None:
         all_trans_vectors = find_all_trans_vector_with_pattern(result_filter,pattern)
         all_trans_vectors.insert(0,(0,0))
-        index_before = index_pattern
         ret_trans_vectors = list()
         for trans in all_trans_vectors:
-
-            first_trans_note = (pattern[0][0]+trans[0],pattern[0][1]+trans[1])
+            first_trans_note = (pattern[0].start + trans[0],pattern[0].pitch + trans[1])
             length_pattern = len(pattern)
             i = 0
             current_pattern = list()
-            while i < len(seq):
+            while i<len(seq):
                 current_note = seq[i]
-                if current_note!=None and current_note[0]==first_trans_note[0] and current_note[1] == first_trans_note[1]:
+                if current_note!=None and current_note.start == first_trans_note[0] and current_note.pitch == first_trans_note[1]:
                     pattern_to_indices[index_pattern] = i
                     for j in range(length_pattern):
-                        trans_note = (pattern[j][0]+trans[0],pattern[j][1]+trans[1])
+                        trans_note = pretty_midi.Note(velocity = 100, start = pattern[j].start + trans[0],end=pattern[j].start + trans[0]+1, pitch = pattern[j].pitch + trans[1])
                         current_pattern.append(trans_note)
                         seq[i+j] = None
                     break
@@ -160,9 +130,7 @@ def find_pattern_with_indices(seq,list_patterns,pattern_to_indices,index_pattern
                 index_pattern+=1
                 ret_trans_vectors.append(trans)
                 list_patterns.append(current_pattern)
-        return pattern,index_pattern,ret_trans_vectors
-    return pattern,index_pattern,None
-
+        return pattern,index_pattern,None
 def is_list_empty(seq):
     for n in seq:
         if n!=None:
@@ -170,29 +138,13 @@ def is_list_empty(seq):
     return True
 
 def find_all_patterns(seq):
-    """
-    finds all patterns in a list of (onset,pitch) tuples.
-    Ex: let a certain sequence be 0,1,2,0,1,2,2,3,4,1,3,1,3
-    Then a first pattern would be [0,1,2], appearing three times, 
-    the third time being shifted up by 2 ([2,3,4]).
-    A second pattern would be [1,3], appearing twice.
-    In this case, the function should return:
-    list_patterns = [[0,1,2],[0,1,2],[2,3,4],[1,3],[1,3]]
-    patterns_to_indices = {
-        0: 0,
-        1: 3,
-        2: 6,
-        3: 9,
-        4: 11
-    }
-    """
     list_patterns = list()
     pattern_to_indices = {}
     index_pattern = 0
     trans_vectors = list()
     while not is_list_empty(seq):
         pattern,index_pattern,all_trans_vectors = find_pattern_with_indices(seq,list_patterns,pattern_to_indices,index_pattern)
-        if pattern==None: # case only one note at the end
+        if pattern==None: # case only one note patterns
             for i in range(len(seq)):
                 if seq[i]!=None:
                     list_patterns.append([seq[i]])
@@ -220,7 +172,7 @@ def collapse_pattern_to_indices(trans_vectors):
                 current_trans = trans_vectors[j]
                 if current_trans[0]==0 and current_trans[1]==0:
                     for key in temp_dict:
-                        collapsed_indices_to_pattern[current_index] = temp_dict[key]
+                        collapsed_indices_to_pattern[current_index] = temp_dict
                         current_index+=1
                     break
                 if current_trans[1] not in temp_dict:
@@ -238,6 +190,7 @@ def transform_collapsed_and_indices(collapsed,pattern_to_indices):
         for val in collapsed[key]:
             true_indices[key].append(pattern_to_indices[val])
     return true_indices
+
 def transform_back_into_seq(true_indices):
     reversed_indices = {}
     for key in true_indices:
@@ -249,13 +202,3 @@ def transform_back_into_seq(true_indices):
     for val in keys:
         seq.append(reversed_indices[val])
     return seq
-def midi_notes_to_tuples(notes):
-    """
-    converts a sequence of pretty_midi notes into a list of (onset,pitch) elements
-    """
-    seq = list()
-    for n in notes:
-        seq.append((n.start,n.pitch))
-    return seq
-def find_patterns_in_notes(patterns,notes):
-    pass
